8c0a51ab4faf434bfbccfee09673e5d62b64ebbe
Seth Mosgin <smo1@umbc.edu>
2025-11-02 13:37:50 -0500

    Initial Commit

---
+++ Pokeder/PokederApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokederApp.swift
+//  Pokeder
+//
+//  Created by Seth Mosgin on 11/2/25.
+//
+
+import SwiftUI
+
+@main
+struct PokederApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PokederTests/PokederTests.swift
@@ -0,0 +1,17 @@
+//
+//  PokederTests.swift
+//  PokederTests
+//
+//  Created by Seth Mosgin on 11/2/25.
+//
+
+import Testing
+@testable import Pokeder
+
+struct PokederTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
